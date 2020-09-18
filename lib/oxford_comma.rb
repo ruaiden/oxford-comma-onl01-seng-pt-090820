f8854dd4e7b5ee91888ab57bac0a18e8f8ad83e0
Aiden Marques <aiden.marques94@gmail.com>
2020-09-18 01:33:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
+
 def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+    
+  elsif array.length == 3
+    array.insert(2, "and ")
+    array.insert(1,", ")
+    array.insert(3, ", ")
+    array.join
+    
+  elsif array.length > 3
+    array << "and"
+    half=array[0..4].join(", ")
+    other=array[5]
+    "#{half}#{other}"
+   
+    
+  else
+    array.join
+  end
 
 end
